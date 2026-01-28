8f73c05155de9869197de96d98b57c170b6d0515
andre-ramos-rodrigues <andreramos@id.uff.br>
2026-01-28 09:06:33 -0300

    prisma client

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,8 +1,8 @@
-import { PrismaClient } from '../generated/prisma/client'
+import { PrismaClient } from '@prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
 
 const globalForPrisma = global as unknown as {
-    prisma: PrismaClient
+    prisma: PrismaClient | undefined
 }
 
 const adapter = new PrismaPg({
